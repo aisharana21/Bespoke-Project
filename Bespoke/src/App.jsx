4f2409be4d539a5346b569033caa477d1f5d02c0
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-11 17:52:38 +0500

    our values

--- Bespoke/src/App.jsx
+++ Bespoke/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { OurMission } from './components/OurMission'
+import { OurValues } from './components/OurValues'
 
 function App() {
 return(  <>
