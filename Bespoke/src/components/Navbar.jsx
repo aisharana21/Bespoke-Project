c0c3e7261c8d5032c4b5497db290c0de0fb79319
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-11 10:34:07 +0500

    make react setup

---
+++ Bespoke/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './CSS/navbar.css'
+export function Navbar(){
+    return(
+<div className="nav-container">
+    <div className="nav">
+<div className="brand-name">
+    Bespoke
+</div>
+<div className="menu">
+    Menu
+</div>
+    </div>
+
+</div>
+
+    );
+}
