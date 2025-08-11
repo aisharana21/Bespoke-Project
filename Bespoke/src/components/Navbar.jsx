eba4c7c3c07a037fae990e3a25af92a2927b58c8
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-11 15:37:59 +0500

    completed our mission part

--- Bespoke/src/components/Navbar.jsx
+++ Bespoke/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import './CSS/navbar.css'
 import MenuImage from '../assets/Menu.png'; 
+import LogoImage from '../assets/logo.png'; 
 import { ProductDesign } from './ProductDesign';
 export function Navbar(){
     return(
 <div className="nav-container">
     <div className="nav">
 <div className="brand-name">
-    Bespoke
+  <span className='logo-image'><img src={LogoImage} alt="" /></span>  Bespoke
 </div>
 <div className="menu">
     MENU <span><img src={MenuImage} alt="" /></span>
