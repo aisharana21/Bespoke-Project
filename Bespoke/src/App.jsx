cecd8e52c34f37bb51b4aa13fa548572e4c9b0b3
AyeshaRana <rana.ayesha590@gmail.com>
2025-08-12 11:54:37 +0500

    completed 3 section and created footer file

--- Bespoke/src/App.jsx
+++ Bespoke/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Navbar } from './components/Navbar'
 import { OurMission } from './components/OurMission'
 import { OurValues } from './components/OurValues'
+import { RequestProduct } from './components/RequestProduct'
 
 function App() {
 return(  <>
   <Navbar/>
     <OurMission/>
 <OurValues/>
+<RequestProduct/>
   </>
 )}
 
